52d5de7ab4a9eb1f4b56042c1dfca39fc000c68d
Sunny-008 <sunnydhanwant4240.in>
2025-01-30 23:07:17 -0800

    add basname in router

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className={style.app}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio">
         <Navbar />
         <Routes>
           <Route path="/" element={<Hero />}></Route>
